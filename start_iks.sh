9b4c3dcfda3415bd9d09718f2978dd19b65076a8
Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>
2020-09-18 12:51:30 +0200

    Update start_iks.sh

--- start_iks.sh
+++ start_iks.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo $IKS_CLUSTER 
-ibmcloud ks cluster ls |grep $IKS_CLUSTER 
+echo $IKS_CLUSTER$BUILD_NUMBER
+ibmcloud ks cluster ls |grep $IKS_CLUSTER$BUILD_NUMBER
 if [[ $? -eq 0 ]]
 then
    echo "."
 else
-   ibmcloud ks cluster create classic --name $IKS_CLUSTER 
+   ibmcloud ks cluster create classic --name $IKS_CLUSTER$BUILD_NUMBER
    sleep 5
 fi
 
-ibmcloud ks cluster ls |grep $IKS_CLUSTER > log.txt
+ibmcloud ks cluster ls |grep $IKS_CLUSTER$BUILD_NUMBER > log.txt
 cat log.txt
 cat log.txt | grep normal
 if [[ $? -eq 0 ]]
