56e6ae409dde011356f5ba149ae35e4a214e8aee
Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>
2020-09-18 15:38:50 +0200

    Update start_iks.sh

--- start_iks.sh
+++ start_iks.sh
@@ -6,8 +6,9 @@ export IKS_CLUSTER_PRIVATE_VLAN=2918270
 export IKS_CLUSTER_PUBLIC_VLAN=2918268
 export IKS_CLUSTER_FLAVOR=u3c.2x4
 export IKS_CLUSTER_TAG_NAMES="owner:artur.bereta,team:CP4MCM,Usage:temp,Usage_desc:'Certification tests',Review_freq:month"
-echo $CN
+echo Cluster which we can ganerate $CN
 
+echo Input cluster $1
 
 if [[ $1 -eq '' ]]
 then
