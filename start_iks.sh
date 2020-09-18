9a04fa577c0d8e2abd3ec0a476387d65bf30dff5
Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>
2020-09-18 13:27:24 +0200

    Update start_iks.sh

--- start_iks.sh
+++ start_iks.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 export RANDFILE=`cat ./random.txt`
 export CN=$IKS_CLUSTER$RANDFILE
+export IKS_CLUSTER_ZONE=dal10
+export IKS_CLUSTER_PRIVATE_VLAN=2887074
+export IKS_CLUSTER_PUBLIC_VLAN=2887072
+export IKS_CLUSTER_FLAVOR=u3c.2x4
 echo $CN
 ibmcloud ks cluster ls |grep $CN
 if [[ $? -eq 0 ]]
 then
    echo "."
 else
-   ibmcloud ks cluster create classic --name $CN --flavor $IKS_CLUSTER_FLAVOR --hardware shared --workers 1 --zone dal10
+   ibmcloud ks cluster create classic --name $CN --flavor $IKS_CLUSTER_FLAVOR --hardware shared --workers 1 --zone $IKS_CLUSTER_ZONE --public-vlan $IKS_CLUSTER_PUBLIC_VLAN --private-vlan $IKS_CLUSTER_PRIVATE_VLAN
    sleep 5
 fi
 
@@ -21,6 +25,6 @@ then
 else
    echo "Wait creating cluster"
    cat log.txt
-   sleep 60
+   sleep 20
    ./start_iks.sh
 fi
