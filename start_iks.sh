ea37d5c32225f285722de0ed588a492193913d05
Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>
2020-09-19 13:14:44 +0200

    Update start_iks.sh

--- start_iks.sh
+++ start_iks.sh
@@ -9,70 +9,52 @@ export IKS_CLUSTER_TAG_NAMES="owner:artur.bereta,team:CP4MCM,Usage:temp,Usage_de
 
 if [ -z "$1" ]
 then
-   echo Cluster which we can ganerate $CN
+   echo "try to find cluster license-service"
+   ibmcloud ks cluster ls |grep license-service  | grep -m1 normal > once.txt
+   if [[ $? -eq 0 ]]
+   then
+      cat once.txt | awk '{print $1}' >  ./clustername.txt
+      export CN=`cat ./clustername.txt`
+      echo "Cluser was found" $CN
+   fi
 else
-  echo Try to use Cluster  $1
+  echo "You choice to use cluster" $1
   echo $1 > ./clustername.txt
-  ibmcloud ks cluster config --cluster $1  --yaml --admin
-  kubectl config current-context
-  kubectl get nodes
-  if [[ $? -ne 0 ]]
-  then
-     echo "ERROR CANNOT GET NODES!!!" 
-  fi
-  kubectl get namespace |grep ibm-common-services
-  while [ $? -eq 0 ] ; do
-       echo "There is namespace ibm-common-services inside cluster. Wait for removing it. Please remove it"
-       sleep 20
-       kubectl get namespace |grep ibm-common-services
-  done  
-  echo "There is not ibm-common-services Namespace. It will be create"
-  kubectl create namespace ibm-common-services
-  exit 0
+  export CN=`cat ./clustername.txt`
 fi
-
-#only once
-ibmcloud ks cluster  ls |grep license-service  | grep -m1 normal > once.txt
-if [[ $? -eq 0 ]]
-then
-   cat once.txt | awk '{print $1}' >  ./clustername.txt
-   export CN=`cat ./clustername.txt`
-fi
-   
+  
 ibmcloud ks cluster ls |grep $CN
 if [[ $? -eq 0 ]]
 then
-   echo "."
+   echo "Cluster exists"
 else
+   echo "Start creating cluster " $CN
    ibmcloud ks cluster create classic --name $CN --flavor $IKS_CLUSTER_FLAVOR --hardware shared --workers 1 --zone $IKS_CLUSTER_ZONE --public-vlan $IKS_CLUSTER_PUBLIC_VLAN --private-vlan $IKS_CLUSTER_PRIVATE_VLAN
-   sleep 5
-fi
-
-ibmcloud ks cluster ls |grep $CN > log.txt
-cat log.txt
-cat log.txt | grep normal
-if [[ $? -eq 0 ]]
-then
+   sleep 10
+   
+   ibmcloud ks cluster ls | grep $CN | grep normal 
+   while [ $? -ne 0 ] ; do
+     echo "Wait for cluster creation"
+     sleep 30
+     ibmcloud ks cluster ls |grep $CN | grep normal 
+   done  
    echo "Cluster was created"
-   ibmcloud ks cluster config --cluster $CN  --yaml --admin
-   kubectl config current-context
-     kubectl get nodes
-     if [[ $? -ne 0 ]]
-     then
-        echo "ERROR CANNOT GET NODES!!!" 
-     fi     kubectl get namespace |grep ibm-common-services
-     while [ $? -eq 0 ] ; do
-          echo "There is namespace ibm-common-services inside cluster. Wait for removing it. Please remove it"
-          sleep 20
-          kubectl get namespace |grep ibm-common-services
-     done  
-     echo "There is not ibm-common-services Namespace. It will be create"
-     kubectl create namespace ibm-common-services
-
-  exit 0
-else
-   echo "Wait creating cluster"
-   cat log.txt
-   sleep 20
-   ./start_iks.sh
 fi
+
+echo "Try to connect to the cluster" $CN
+ibmcloud ks cluster config --cluster $CN  --yaml --admin
+kubectl config current-context
+kubectl get nodes
+if [[ $? -ne 0 ]]
+then
+  echo "ERROR CANNOT GET NODES!!!" 
+  exit 1
+fi
+kubectl get namespace |grep ibm-common-services
+while [ $? -eq 0 ] ; do
+    echo "There is namespace ibm-common-services inside cluster. Wait for removing it. Please remove it"
+    sleep 20
+    kubectl get namespace |grep ibm-common-services
+done  
+echo "There is not ibm-common-services Namespace. It will be create"
+kubectl create namespace ibm-common-services
