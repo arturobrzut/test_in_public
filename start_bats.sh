3066e75f5d6262349e7c8b9200508b1bfb025052
Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>
2020-09-20 15:31:45 +0200

    Create start_bats.sh

---
+++ start_bats.sh
@@ -0,0 +1,9 @@
+mkdir ./lib
+cd lib
+wget https://raw.githubusercontent.com/bats-core/bats-detik/master/lib/detik.bash
+wget https://raw.githubusercontent.com/bats-core/bats-detik/master/lib/linter.bash
+wget https://raw.githubusercontent.com/bats-core/bats-detik/master/lib/utils.bash
+chmod +x *.bash
+cd ..
+chmod +x *.bash
+bats ./tests.bats
